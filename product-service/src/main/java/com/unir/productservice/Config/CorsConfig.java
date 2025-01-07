4aa5e396013882810bef7d9f347dcbf0227bfc1d
Rafael Suarez <rafasuarez0202@gmail.com>
2025-01-07 15:46:05 -0500

    ajustes product service delete cors

--- product-service/src/main/java/com/unir/productservice/Config/CorsConfig.java
+++ product-service/src/main/java/com/unir/productservice/Config/CorsConfig.java
@@ -14,11 +14,23 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // Origen permitido
+
+        // Origen permitido
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://8080-old-forest-22658171.in-ws1.runcode.io"));
+
+        // Métodos permitidos
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        // Encabezados permitidos
         config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+
+        // Permitir credenciales
         config.setAllowCredentials(true);
 
+        // Permitir encabezados adicionales en la respuesta
+        config.addExposedHeader("Access-Control-Allow-Origin");
+
+        // Configurar el origen basado en las rutas
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config); // Aplica a todas las rutas
 
