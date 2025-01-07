bd59ae38ea8388f09e9b6c711af15fe4b24ba42e
Javier Díaz Díaz <jddiazsena@gmail.com>
2025-01-07 13:30:17 -0500

    ajuste

--- product-service/src/main/java/com/unir/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/unir/productservice/controller/ProductController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping("/api/products")
-
+@CrossOrigin(origins = "*")
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
