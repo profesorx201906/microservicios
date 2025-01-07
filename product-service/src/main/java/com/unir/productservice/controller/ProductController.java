411f100f5c1a5063939c3743179ce4e1d333be36
Rafael Suarez <rafasuarez0202@gmail.com>
2025-01-07 15:24:37 -0500

    ajustes product service delete

--- product-service/src/main/java/com/unir/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/unir/productservice/controller/ProductController.java
@@ -10,7 +10,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +35,13 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public String createProduct(@RequestBody Product product) {
         productRepository.save(product);
+        return "ok";
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public String deleteProduct(@PathVariable("id") String id){
+        productRepository.deleteById(id);
         return "ok";
     }
 
